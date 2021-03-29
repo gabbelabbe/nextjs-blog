381b4fde72f7cd5fbffd89ecd9523c0f7009f19e
gabbelabbe <gabbe.stark@gmail.com>
2021-03-29 21:48:59 +0200

    test with dynamic data

--- pages/chart.js
+++ pages/chart.js
@@ -1,14 +1,34 @@
 import { BarChart, XAxis, YAxis, Bar, Tooltip, ResponsiveContainer, Cell, Legend } from 'recharts'
 import Layout from '../components/layout'
 import { getGithubData } from '../lib/github'
+import useSWR from 'swr'
 
-export default function Chart({ postData }) {
+const fetcher = (...args) => fetch(...args).then(res => res.json()).then(res => {
+  const postData = []
+  res.forEach((data) => {
+    if (data.language) {
+      const i = postData.findIndex(val => val.name === data.language)
+      if (i !== -1) {
+        postData[i].value += 1
+      } else {
+        postData.push({name: data.language, value: 1})
+      }
+    }
+  })
+  return postData
+})
+
+export default function Chart() {
+  const { data: githubData, error } = useSWR('https://api.github.com/users/gabbelabbe/repos', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!githubData) return <div>loading...</div>
 
   return (
     <Layout>
       <div style={{ height: 500 }}>
         <ResponsiveContainer height='100%' width='100%'>
-          <BarChart data={postData} cursor='pointer'>
+          <BarChart data={githubData} cursor='pointer'>
             <XAxis dataKey='name' />
             <YAxis />
             <Legend height={25} iconSize={0} />
@@ -20,24 +40,4 @@ export default function Chart({ postData }) {
       </div>
     </Layout>
   )
-}
-
-export async function getStaticProps() {
-  const githubData = await getGithubData()
-  const postData = []
-  githubData.forEach((data) => {
-    if (data.language) {
-      const i = postData.findIndex(val => val.name === data.language)
-      if (i !== -1) {
-        postData[i].value += 1
-      } else {
-        postData.push({name: data.language, value: 1})
-      }
-    }
-  })
-  return {
-    props: {
-      postData
-    }
-  }
 }
